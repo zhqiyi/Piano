cc31cda446b6aa9e709d9a4711588c6636e76e62
不及格的程序员-八神 <wellingsok@hotmail.com>
2014-06-14 08:46:30 +0800

    `

---
+++ PianoHelp/MelodyButton.h
@@ -0,0 +1,13 @@
+//
+//  MelodyButton.h
+//  PianoHelp
+//
+//  Created by Jobs on 14-6-13.
+//  Copyright (c) 2014年 FlintInfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MelodyButton : UIButton
+@property(nonatomic, strong) NSString* fileName;
+@end
---
+++ PianoHelp/MelodyButton.m
@@ -0,0 +1,31 @@
+//
+//  MelodyButton.m
+//  PianoHelp
+//
+//  Created by Jobs on 14-6-13.
+//  Copyright (c) 2014年 FlintInfo. All rights reserved.
+//
+
+#import "MelodyButton.h"
+
+@implementation MelodyButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
